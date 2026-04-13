14242bc10f17cb64ff10518f4c58380b247dc6d2
keppler_dev <sergio@tecknum.com>
2026-04-13 15:02:56 -0600

    Ajsutes de Modulo de Video

--- src/app/tips/page.jsx
+++ src/app/tips/page.jsx
@@ -10,6 +10,23 @@ import { tipsparapintar, contenidoTips } from '@/components/server';
 import Image from 'next/image';
 import ReactPlayer from 'react-player';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const Contenedor = styled.iframe`
+  width: 100%;
+
+  @media (min-width: 300px) {
+    height: 1200px;
+  }
+
+  @media (min-width: 800px) {
+    height: 1200px;
+  }
+
+  @media (min-width: 1300px) {
+    height: 1200px;
+  }
+`;
 
 const TipsParaPintar = () => {
   const [windowWidth, setWindowWidth] = useState(0);
@@ -55,111 +72,16 @@ const TipsParaPintar = () => {
           <Typography variant="h1" className="uppercase">
             Tutoriales y Tips
           </Typography>
-          <ul className="p-2">
-            {tipsparapintar?.map((item, index) => (
-              <li
-                className="mt-4 cursor-pointer p-2 hover:bg-white text-black 
-                            text-xl transition-transform transform duration-500 hover:scale-105 uppercase"
-                key={index}
-                onClick={() => handleTips(item.target)}
-              >
-                {item.name}
-              </li>
-            ))}
-          </ul>
         </div>
-        <div
-          className={`w-full grid gap-10 ${
-            windowWidth < 860
-              ? 'grid-cols-1'
-              : windowWidth < 1280
-              ? 'grid-cols-2'
-              : 'grid-cols-3'
-          }`}
-        >
-          {tuoriales?.map((item, index) =>
-            item.video ? (
-              <Card
-                key={index}
-                className={`mt-6 ${windowWidth < 860 ? ' w-[70%]' : 'w-[90%]'}`}
-              >
-                <CardHeader color="blue-gray" className="relative h-[30vh]">
-                  <div key={index} className="">
-                    <ReactPlayer
-                      url={item.video}
-                      width={'auto'}
-                      height={windowWidth < 1000 ? 200 : 300}
-                      loop
-                      controls
-                      className={'w-full'}
-                    />
-                  </div>
-                </CardHeader>
-                <CardBody>
-                  <div className="w-full flex items-center">
-                    <div className="w-full">
-                      <Typography
-                        variant="h4"
-                        color="blue-gray"
-                        className="ml-2 mb-4 uppercase"
-                      >
-                        {item.name}
-                      </Typography>
-                      {item.enlace == null ? null : (
-                        <Link
-                          href={item.enlace}
-                          className="ml-2 hover:border-b border-[#c50411]"
-                          onClick={() =>
-                            handleLocal(item.categoria, item.microcategoria)
-                          }
-                        >
-                          Ver Producto
-                        </Link>
-                      )}
-                    </div>
-                  </div>
-
-                  <Typography>{item.description}</Typography>
-                </CardBody>
-              </Card>
-            ) : (
-              <Card
-                key={index}
-                className={`mx-auto mt-6 ${
-                  windowWidth < 860 ? ' w-[70%]' : 'w-[90%]'
-                }`}
-              >
-                <CardHeader color="blue-gray" className="relative ">
-                  <Image
-                    src={item.imagen}
-                    width={500}
-                    height={600}
-                    className={`${item.causas ? 'w-[30%]' : 'w-full'}`}
-                    alt="vinilicas"
-                  ></Image>
-                </CardHeader>
-                <CardBody>
-                  <div className="w-full flex items-center">
-                    <div
-                      className={`rounded-full h-[15px] w-[15px]`}
-                      style={{ backgroundColor: item.color }}
-                    ></div>
-                    <div className="w-full">
-                      <Typography
-                        variant="h5"
-                        color="blue-gray"
-                        className="ml-2 uppercase"
-                      >
-                        {item.title}
-                      </Typography>
-                    </div>
-                  </div>
-
-                  <Typography>{item.description}</Typography>
-                </CardBody>
-              </Card>
-            )
-          )}
+        <div>
+          <Contenedor
+            // src="http://localhost/ipesa_video/index.php"
+            src="https://tecknum.com/ipesa_video/index.php"
+            title="Mi Iframe"
+            frameborder="0"
+            rameborder="0"
+            // scrolling="no"
+          />
         </div>
       </div>
     </div>
